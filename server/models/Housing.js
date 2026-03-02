fdbd4e12dd06ec0cbdeacc443efc36b8cdf901e9
Mema <patty_60071027@outlook.es>
2026-03-02 13:34:53 -0500

    feat: add housing model

---
+++ server/models/Housing.js
@@ -0,0 +1,121 @@
+import mongoose from "mongoose";
+
+const housingSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      immutable: true,
+      trim: true
+    },
+
+    area: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    bedrooms: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    bathrooms: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    parkingSpaces: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    floors: {
+      type: Number,
+      required: true,
+      min: 1
+    },
+
+    priceInSoles: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    priceInDollars: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 10
+    },
+
+    realEstateProject: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    propertyType: {
+      type: String,
+      required: true,
+      enum: ["Departamento", "Casa", "Dúplex", "Minidepartamento"]
+    },
+
+    department: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    province: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    district: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5
+    },
+
+    imageUrl: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    status: {
+      type: String,
+      enum: ["Disponible", "Reservado", "Vendido"],
+      default: "Disponible"
+    }
+  },
+  {
+    timestamps: true,
+    strict: "throw"
+  }
+);
+
+housingSchema.index({ code: 1 }, { unique: true });
+
+const Housing = mongoose.model("Housing", housingSchema);
+
+export default Housing;
